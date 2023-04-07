54935653bc5761c365afdb0dd8026bde92f65210
choi <bcaca0831@gmail.com>
2023-04-07 12:51:35 +0900

    jquery 모달창 만들기

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/** @format */
+
+window.onload = function () {
+  // 멀티미디어 로드완료시 실행
+};
+$(document).ready(function () {
+  // document.querySelector.
+  $("body").addClass("modalActive");
+  // 문서로딩 완료(js, css, html)
+  // 모달창 기능
+  //   const mbt = document.querySelector(".modalBt")
+  //   mbt.addEventListener("click", function(){
+  //   });
+  const mbt = $(".modalBt");
+  const mWin = $(".modalWin");
+  //   const mbtClose = $(".modalWin a");
+  const mbtClose = mWin.find("a");
+
+  mbt.click(function (e) {
+    e.preventDefault();
+    // 캡처링 및 버블링 막기
+    e.stopPropagation();
+    // display: block;
+    // mWin.show();
+    mWin.fadeIn(300);
+  });
+  mbtClose.click(function (e) {
+    e.preventDefault();
+    // 캡처링 및 버블링 막기
+    e.stopPropagation();
+    mWin.fadeOut(300);
+    $("body").removeClass("modalActive");
+  });
+
+  //   mWin.fadeIn(300);
+  mWin.show();
+});
